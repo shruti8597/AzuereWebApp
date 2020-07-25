db3b0f70ad677a8cf8a9d038049138b8537e6322
Shruti <shrutiagrawal915@gmail.com>
2020-07-25 10:50:04 +0530

    Inirial Revert

--- server.js
+++ server.js
@@ -1,25 +1,12 @@
-// const http = require('http');
-
-// const server = http.createServer((request, response) => {
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.end("Hello World!");
-// });
-
-// const port = process.env.PORT || 1337;
-// server.listen(port);
-
-// console.log("Server running at http://localhost:%d", port);
-
 const express = require('express');
-// const path = require('path');
+const path = require('path');
 
 const app = express();
 
-// app.use(express.static(path.resolve(__dirname, 'dist')));
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.use((req, res) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World!');
+  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
 app.listen(5000, () => {
